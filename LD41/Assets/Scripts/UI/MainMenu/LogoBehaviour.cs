dfbd59b474d5a03e4a59ec0082c749d8d1c5c7cf
Luke Trynchuk <luke123t@gmail.com>
2018-04-21 15:16:17 -0300

    Add Logo SFX.

---
+++ LD41/Assets/Scripts/UI/MainMenu/LogoBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace LD.UI
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class LogoBehaviour : MonoBehaviour
+    {
+
+        #region Private Variables
+        [SerializeField]
+        private AudioClip m_bounceSFX;
+
+        private AudioSource m_source;
+		#endregion
+
+		#region Main Methods
+		private void Start()
+		{
+            m_source = GetComponent<AudioSource>();
+		}
+
+		public void PlayBounce()
+        {
+            m_source.clip = m_bounceSFX;
+            m_source.Play();
+        }
+        #endregion
+    }
+}
