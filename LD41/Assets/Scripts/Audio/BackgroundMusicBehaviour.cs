a88da8602dc34ff3d8f2d6ec630cc6300732ed0c
Luke Trynchuk <luke123t@gmail.com>
2018-04-21 14:24:35 -0300

    Add Background music transitioning.

---
+++ LD41/Assets/Scripts/Audio/BackgroundMusicBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+namespace LD.Audio
+{
+    /// <summary>
+    /// This component will act as a background music player. Given an array
+    /// of music clips, choose one to play and loop.
+    /// </summary>
+
+    [RequireComponent(typeof(AudioSource))]
+    [RequireComponent(typeof(TransitionAudioMixer))]
+    public class BackgroundMusicBehaviour : MonoBehaviour
+    {
+
+        #region Private Variables
+        [SerializeField]
+        private AudioClip[] m_musicClips;
+
+        private AudioSource m_source;
+        private TransitionAudioMixer m_transitionAudio;
+        #endregion
+
+        #region Main Methods
+        private void Start()
+        {
+            InitializeComponents();
+
+            StartMusic();
+
+            m_transitionAudio.SetTransitionTime(2f);
+            m_transitionAudio.Transition();
+        }
+        #endregion
+
+        #region Utility Methods
+
+        private void StartMusic()
+        {
+            m_source.clip = GetAudioClipToPlay();
+
+            if (m_source.clip == null)
+            {
+                Debug.LogError("Unable to play music from empty list.");
+                return;
+            }
+
+            m_source.loop = true;
+            m_source.Play();
+        }
+
+        private void InitializeComponents()
+        {
+            m_source = GetComponent<AudioSource>();
+            m_transitionAudio = GetComponent<TransitionAudioMixer>();
+        }
+
+        private AudioClip GetAudioClipToPlay()
+        {
+            int index = UnityEngine.Random.Range(0, m_musicClips.Length - 1);
+            return m_musicClips[index];
+        }
+        #endregion
+    }
+}
---
+++ LD41/Assets/Scripts/Audio/TransitionAudioMixer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace LD.Audio
+{
+    public class TransitionAudioMixer : MonoBehaviour
+    {
+        #region Public Variables
+        public AudioMixerSnapshot m_TransitionToGroup;
+        #endregion
+
+        #region Private Variables
+        private float m_timeToTransition = 1.0f;
+        #endregion
+
+        #region Main Methods
+        public void Transition()
+        {
+            if(m_TransitionToGroup == null)
+            {
+                Debug.LogError("Cannot transition to a null audio snapshot");
+                return;
+            }
+
+            m_TransitionToGroup.TransitionTo(m_timeToTransition);
+        }
+
+        public void SetTransitionTime(float time)
+        {
+            if (time < 0.0f)
+                return;
+
+            m_timeToTransition = time;
+        }
+        #endregion
+
+    }
+}
