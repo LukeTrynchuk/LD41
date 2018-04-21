e8766a1cdc8c06733fc03e8ab3fc5164a0a78bd2
Luke Trynchuk <luke123t@gmail.com>
2018-04-21 12:17:10 -0300

    Add Start And Quit Button Functionality.

---
+++ LD41/Assets/Scripts/General/AdvanceToNextScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LD.General
+{
+    public class AdvanceToNextScene : MonoBehaviour
+    {
+        #region Main Methods
+        public void Advance()
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            int index = scene.buildIndex;
+            index++;
+            SceneManager.LoadScene(index);
+        }
+        #endregion
+
+    }
+}
--- LD41/Assets/Scripts/Scene/TransitionToGameScene.cs
+++ LD41/Assets/Scripts/Scene/TransitionToGameScene.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using LD.General;
 
 namespace LD.SceneManagement
 {
+    [RequireComponent(typeof(AdvanceToNextScene))]
     public class TransitionToGameScene : MonoBehaviour
     {
 		#region Main Methods
 		private void LateUpdate()
 		{
-            SceneManager.LoadScene(1);
+            AdvanceToNextScene advance = GetComponent<AdvanceToNextScene>();
+            advance.Advance();
 		}
 		#endregion
 	}
