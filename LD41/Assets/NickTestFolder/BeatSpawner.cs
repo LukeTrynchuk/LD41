e20703713884499d54afb76e8a88fca86c181b6c
Nicholas Comeau <nick_comeau3@hotmail.com>
2018-04-21 20:53:38 -0300

    Beat spawning and destroying

---
+++ LD41/Assets/NickTestFolder/BeatSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatSpawner : MonoBehaviour {
+
+    public GameObject beat;
+    public float setTimer;
+    float countDownTimer;
+	// Use this for initialization
+	void Start () {
+        countDownTimer = setTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(countDownTimer<=0)
+        {
+            GameObject newBeat = Instantiate(beat, new Vector3(0,0,0), Quaternion.identity);
+            newBeat.transform.SetParent(this.gameObject.transform);
+            newBeat.transform.position = transform.position;
+            countDownTimer = setTimer;
+        }
+
+        countDownTimer -= Time.deltaTime;
+	}
+}
