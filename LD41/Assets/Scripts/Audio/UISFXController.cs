4672b71881ffba9c0abf682a210b147c0f21c2d5
Luke Trynchuk <luke123t@gmail.com>
2018-04-21 15:05:27 -0300

    Add Menu Button Audio SFX.

---
+++ LD41/Assets/Scripts/Audio/UISFXController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace LD.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class UISFXController : MonoBehaviour
+    {
+        #region Private Variables
+        [SerializeField]
+        private AudioClip m_buttonClickAudioClip;
+
+        private AudioSource m_source;
+		#endregion
+
+		#region Main Methods
+		private void Start()
+		{
+            m_source = GetComponent<AudioSource>();
+		}
+
+		public void ButtonClicked()
+        {
+            m_source.clip = m_buttonClickAudioClip;
+            m_source.pitch = Random.Range(0.9f, 1.1f);
+            m_source.Play();
+        }
+        #endregion
+    }
+}
