ea23e8737e2d2c2cb8bd2c6a3e886c8186707ad3
Luke Trynchuk <luke123t@gmail.com>
2018-04-22 02:09:49 -0300

    Add Move To Functionality to the Reaper animation helper script.

--- LD41/Assets/Scripts/Animation/ReaperAnimationHelper.cs
+++ LD41/Assets/Scripts/Animation/ReaperAnimationHelper.cs
@@ -1,12 +1,143 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
 
 namespace LD.Animation
 {
     [RequireComponent(typeof(Animator))]
-    public class ReaperAnimationHelper : MonoBehaviour 
+    public class ReaperAnimationHelper : MonoBehaviour
     {
+        #region Public Variables
+        public System.Action OnArrivedToDestination;
+        #endregion
+
+        #region Private Variables
+        [SerializeField]
+		private float m_timeToMove;
+
+        [SerializeField]
+        private float m_timeToTurn;
+
+        private Animator m_animator;
+        private bool m_alreadyMoving = false;
+
+        //Animator bool names
+        private const string m_moveForward = "Move Forward";
+        private const string m_jump = "Jump";
+        private const string m_spinAttack = "Spin Attack";
+
+        //Animator trigger names
+        private const string m_slash1 = "Slack Attack 01";
+        private const string m_slash2 = "Slack Attack 02";
+        private const string m_die = "Die";
+		#endregion
+
+		#region Main Methods
+		private void Start()
+		{
+            m_animator = GetComponent<Animator>();
+		}
+
+        public void MoveTo(Vector3 positionToMoveTo)
+        {
+            if (m_alreadyMoving)
+            {
+                Debug.Log("Reaper already moving. Wait for reaper to finish " +
+                          "moving before giving it a new destination to move to.");            
+				return;
+            }
+
+            StartCoroutine(Move(positionToMoveTo));
+        }
+
+        public void SetToDead()
+        {
+            ReturnToIdle();
+            m_animator.SetBool(m_die, true);
+        }
+
+		#endregion
+
+		#region Utility Methods
+		void ReturnToIdle()
+        {
+            m_animator.SetBool(m_moveForward, false);
+            m_animator.SetBool(m_jump, false);
+            m_animator.SetBool(m_spinAttack, false);
+        }
+
+        void BeginMovingAnimation()
+        {
+            ReturnToIdle();
+            m_animator.SetBool(m_moveForward, true);
+        }
+
+        private IEnumerator Move(Vector3 pos)
+		{
+            m_alreadyMoving = true;
+
+            Vector3 attractionVec = transform.position - pos;
+            Quaternion desiredQuat = GetRotationQuaternion(pos);
+
+            float m_startTime = 0f;
+            Quaternion originalQuat = transform.rotation;
+
+            while(m_startTime <= m_timeToTurn)
+            {
+				m_startTime += Time.deltaTime;
+    
+                Quaternion rot = originalQuat;
+                rot = Quaternion.Slerp(rot, desiredQuat, m_startTime / m_timeToTurn);
+                transform.rotation = rot;
+                yield return null;
+
+            }
 
 
 
-    }
+            m_startTime = 0f;
+            Vector3 originalPosition = transform.position;
+
+            while(Vector3.Distance(pos, transform.position) >= 0.05f)
+            {
+				m_startTime += Time.deltaTime;
+                transform.position = Vector3.Lerp(originalPosition, pos, m_startTime / m_timeToMove);
+                yield return null;
+            }
+
+
+
+            FireDoneEvent();
+
+            m_alreadyMoving = false;
+		}
+
+        private Quaternion GetRotationQuaternion(Vector3 desirePosition)
+        {
+            Vector3 attractionVec = desirePosition - transform.position;
+
+            float angle = Vector3.Angle(transform.forward, attractionVec);
+            float rightAngle = Vector3.Angle(transform.right, attractionVec);
+
+            if (angle < rightAngle)
+            {
+                angle *= -1;
+            }
+
+            Quaternion desiredQuat = Quaternion.Euler(new Vector3(0, angle, 0));
+            return desiredQuat;
+        }
+
+		private void FireDoneEvent()
+        {
+            if (OnArrivedToDestination == null)
+                return;
+
+            OnArrivedToDestination();
+        }
+
+        #endregion
+
+	}
 }
---
+++ LD41/Assets/Scripts/Temp/TestAnimatorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LD.Animation;
+
+public class TestAnimatorController : MonoBehaviour 
+{
+
+    public ReaperAnimationHelper m_reaper;
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.P))
+        {
+            m_reaper.MoveTo(new Vector3(0, 0, -1));
+        }
+	}
+}
