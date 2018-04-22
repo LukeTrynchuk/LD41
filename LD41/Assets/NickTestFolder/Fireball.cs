c4e969b11c50994f40876f4461e502dc51938ee8
Nicholas Comeau <nick_comeau3@hotmail.com>
2018-04-22 17:56:13 -0300

    Enemy Spawn

--- LD41/Assets/NickTestFolder/Fireball.cs
+++ LD41/Assets/NickTestFolder/Fireball.cs
@@ -7,9 +7,11 @@ public class Fireball : MonoBehaviour {
 
     float lifeTimer = 3;
     KillCounter_Behaviour killCounter;
+    GameObject environment;
 	// Use this for initialization
 	void Start () {
         killCounter = GameObject.FindWithTag("KillCounter").GetComponent<KillCounter_Behaviour>();
+        environment = GameObject.FindWithTag("Environment");
 	}
 	
 	// Update is called once per frame
@@ -26,6 +28,10 @@ public class Fireball : MonoBehaviour {
         if(other.tag == "Enemy")
         {
             killCounter.Increment(1);
+            GameObject enemyObj = Instantiate(other.gameObject, new Vector3(10, 10, 10), Quaternion.identity);
+            enemyObj.transform.parent = environment.transform;
+            enemyObj.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+            enemyObj.GetComponent<EnemyAI>().position = new Vector2((int)Random.Range(0, 5), (int)Random.Range(0, 5));
             Destroy(other.gameObject);
         }
     }
