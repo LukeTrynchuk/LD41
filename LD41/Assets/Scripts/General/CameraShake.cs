f2c2331ebbbef03102b29197a843649f5345fa24
Luke Trynchuk <luke123t@gmail.com>
2018-04-22 12:43:11 -0300

    Add Camera with Screen shake as prefab.

---
+++ LD41/Assets/Scripts/General/CameraShake.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+namespace LD.General
+{
+    public class CameraShake : MonoBehaviour
+    {
+
+        #region Private Variables
+        private CameraShaker m_shaker;
+        #endregion
+
+        #region Main Methods
+        void Start()
+        {
+            m_shaker = GetComponent<CameraShaker>();
+            if (m_shaker == null)
+            {
+                m_shaker = gameObject.AddComponent<CameraShaker>();
+            }
+        }
+
+        public void Shake(float intensity)
+        {
+            CameraShaker.Instance.ShakeOnce(4f * intensity, intensity, 0.2f, 0.3f);
+        }
+        #endregion
+    }
+}
