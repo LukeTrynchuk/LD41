21d93132776b964036e215ade514ed3054ee1ad4
Luke Trynchuk <luke123t@gmail.com>
2018-04-22 02:43:47 -0300

    Add Rearange Toon Effects in project.

--- LD41/Assets/Scripts/Temp/TestAnimatorController.cs
+++ LD41/Assets/Scripts/Temp/TestAnimatorController.cs
@@ -12,7 +12,8 @@ public class TestAnimatorController : MonoBehaviour
 	{
 		if(Input.GetKeyDown(KeyCode.P))
         {
-            m_reaper.SetToDead();
+            //m_reaper.SetToDead();
+            m_reaper.MoveTo(this.transform.position);
         }
 	}
 }
