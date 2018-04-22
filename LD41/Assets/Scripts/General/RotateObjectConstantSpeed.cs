975e5ba793bb5df5141740c3b7136df1fd04574b
Luke Trynchuk <luke123t@gmail.com>
2018-04-21 22:37:49 -0300

    Add Rotating Clouds.

---
+++ LD41/Assets/Scripts/General/RotateObjectConstantSpeed.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace LD.General
+{
+    public class RotateObjectConstantSpeed : MonoBehaviour
+    {
+        #region Private Variables
+        [SerializeField]
+        private float m_rotateSpeed;
+        #endregion
+
+        #region Main Methods
+        void Update()
+        {
+            transform.rotation *= Quaternion.Euler(Vector3.up * m_rotateSpeed);
+        }
+        #endregion
+    }
+}
